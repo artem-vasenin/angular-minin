ebc0e1492bbfa6abf7ed0dc0558df89aff7dd5ba
Rusich <artem.skobar@yandex.ru>
2020-06-06 22:15:56 +0300

    Pipes base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {PostComponent} from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
   ],
   imports: [
     BrowserModule
