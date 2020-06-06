c64364821f129fb68cbac7186106f2dc3172d76c
Rusich <artem.skobar@yandex.ru>
2020-06-06 18:55:49 +0300

    Bindings

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -7,4 +7,12 @@ import {Component} from '@angular/core';
 })
 export class PostComponent  {
   title = 'Post component';
+  imgNg = 'https://i09.fotocdn.net/s108/8e2c8aea5a2895c7/public_pin_l/2363267384.jpg';
+  imgVue = 'https://images.universe.com/3ec98038-5c4f-4c14-9f38-3975ea9027af/-/progressive/yes/-/inline/yes/';
+
+  constructor() {
+    setTimeout(() => {
+      this.imgNg = this.imgVue;
+    }, 5000);
+  }
 }
