a7da9dc00068a5725181a908483612b842aab62a
Rusich <artem.skobar@yandex.ru>
2020-06-06 23:03:37 +0300

    Components - details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
 
+export interface IPost {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  posts: IPost[] = [
+    {title: 'JS', text: 'Learn JavaScript'},
+    {title: 'Angular', text: 'Learn Angular 9'},
+    {title: 'React Native', text: 'Learn RN'},
+    {title: 'NodeJs', text: 'Learn Node'},
+  ];
+}
