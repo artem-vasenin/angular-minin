7d4e0f73f6487df40971e3bd213717246dfed993
Rusich <artem.skobar@yandex.ru>
2020-06-06 19:32:17 +0300

    Directives

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -9,6 +9,7 @@ export class PostComponent  {
   title = 'Post component';
   imgNg = 'https://i09.fotocdn.net/s108/8e2c8aea5a2895c7/public_pin_l/2363267384.jpg';
   imgVue = 'https://images.universe.com/3ec98038-5c4f-4c14-9f38-3975ea9027af/-/progressive/yes/-/inline/yes/';
+  imgStyles = {width: '200px', height: '200px'};
 
   constructor() {
     setTimeout(() => {
