13bf1f5d376a76668822afb2bf1be12df7a26834
Rusich <artem.skobar@yandex.ru>
2020-06-09 16:55:49 +0300

    Pipes - async

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
 
 export interface IPost {
   title: string;
@@ -11,7 +12,7 @@ export interface IPost {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   posts: IPost[] = [
     {title: 'JS', text: 'Learn JavaScript'},
     {title: 'Angular', text: 'Learn Angular 9'},
@@ -19,9 +20,26 @@ export class AppComponent {
     {title: 'NodeJs', text: 'Learn Node'},
   ];
   search = '';
-  date = new Date();
+  prom: Promise<string> = new Promise<string>(resolve => {
+    setTimeout(() => resolve('promise finish'), 4000);
+  });
+  // для пайпа асинх достаточно этого обсервера
+  date: Observable<Date> = new Observable<Date>(obs => {
+    setInterval(() => obs.next(new Date()), 1000);
+  });
+  // без пайпа необходимо обработать его через стрим в хуке инициализации
+  date$: Observable<Date> = new Observable<Date>(obs => {
+    setInterval(() => obs.next(new Date()), 1000);
+  });
+  date2: Date;
 
   addPost(post: IPost) {
     this.posts.unshift(post);
+  }
+
+  ngOnInit(): void {
+    this.date$.subscribe(date => {
+      this.date2 = date;
+    });
   }
 }
